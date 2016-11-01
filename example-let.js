1a7d7d7c4f6b84b90da2a8d558e1ddb394653ff0
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-02 07:55:50 +0900

    Add the let declared error example

--- example-let.js
+++ example-let.js
@@ -7,3 +7,11 @@ console.log("a = %d, b = %d", a, b);
   console.log("a = %d, b = %d", a, b);
 }
 console.log("a = %d, b = %d", a, b);
+
+try {
+  let c = 3;
+  let c = 4;
+}
+catch (e) {
+  console.log(e);
+}
