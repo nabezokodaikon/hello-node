8de8b7d9d0493a2d41b0d43a83f54ba108acb5fc
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-02 07:49:56 +0900

    Add the const scope example

--- example-const.js
+++ example-const.js
@@ -1,2 +1,16 @@
 const a = 1;
-a = 2;
+const b = 1;
+console.log("a = %d, b = %d", a, b);
+{
+  const a = 2;
+  const b = 2;
+  console.log("a = %d, b = %d", a, b);
+}
+console.log("a = %d, b = %d", a, b);
+
+try {
+  a = 2;
+}
+catch (e) {
+  console.log(e);
+}
