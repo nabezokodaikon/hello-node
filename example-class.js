dbd8c747ef84c8470b54a4547717544378008935
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-02 07:59:34 +0900

    Add the class example

---
+++ example-class.js
@@ -0,0 +1,25 @@
+function Before(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Before.prototype.getArea = function() {
+  return this.x * this.y;
+}
+
+var b = new Before(2, 3);
+console.log("b.x = %d, b.y = %d, b.area = %d", b.x, b.y, b.getArea());
+
+class After {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getArea() {
+    return this.x * this.y;
+  }
+}
+
+var a = new After(4, 5);
+console.log("a.x = %d, a.y = %d, a.area = %d", a.x, a.y, a.getArea());
